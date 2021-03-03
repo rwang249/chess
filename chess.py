59252dfa0569877e042cc2d06c779e49d8c15ddc
rwang <you@example.com>
2021-03-03 04:50:12 -0500

    added inheritance between generic piece class and actual piece class

--- chess.py
+++ chess.py
@@ -11,18 +11,71 @@
 
 class Piece():
     #initalize piece
-    def __init__(self, side, startingLocation, displayValue, scoreValue):
-        print('test')
+    def __init__(self, location, displayValue):
+        startLocation = location
+        print(location)
         
 class Pawn(Piece):
+    #define movement to pass to move
+    def __init__(self, side, location, displayValue, scoreValue):
+        super().__init__(location, displayValue)
+
+    def move(self):
+        print('placeholder for move')
+
+class Knight(Piece):
+    def __init__(self, side, location, displayValue, scoreValue):
+        super().__init__(location, displayValue)
+
     #define movement to pass to move
     def move(self):
         print('placeholder for move')
 
+class Rook(Piece):
+    def __init__(self, side, location, displayValue, scoreValue):
+        super().__init__(location, displayValue)
+
+    #define movement to pass to move
+    def move(self):
+        print('placeholder for move')
+
+class Queen(Piece):
+    def __init__(self, side, location, displayValue, scoreValue):
+        super().__init__(location, displayValue)
+
+    #define movement to pass to move
+    def move(self):
+        print('placeholder for move')
+
+class Bishop(Piece):
+    def __init__(self, side, location, displayValue, scoreValue):
+        super().__init__(location, displayValue)
+
+    #define movement to pass to move
+    def move(self):
+        print('placeholder for move')
+
+class King(Piece):
+    def __init__(self, side, location, displayValue, scoreValue):
+        super().__init__(location, displayValue)
+
+    #define movement to pass to move
+    def move(self):
+        print('placeholder for move')
+
+
 class Chess_Board():
     def __init__(self):
-        board = [['    '] * 8] * 8
-        self.displayBoard(board)
+        board = []
+        for row in range(8):
+            column = []
+            board.append(column)
+            for columnNum in range(8):
+                column.append('.')
+        board[0][7] = 'pawn'
+        #board[1][2] = 'pawn'
+        print(board)
+        #self.displayBoard(board)
         
     def displayBoard(self, board):
         rowNum = 8
@@ -39,31 +92,84 @@ class Chess_Board():
         for letter in ["a", "b", "c", "d", "e", "f", "g", "h"]:
             print("   {} ".format(letter), end="")
         print("\n")
-        
+
+
 # Pawn: 1 point (or pawn)
 # Knight: 3 points.
 # Bishop: 3 points.
 # Rook: 5 points.
 # Queen: 9 points
 #Create pieces
-bp = Piece(self, "b", [x, 7], bp, 1)
-wp = Piece(self, "w", [x, 2], wp, 1)
-br = Piece(self, "b", [1, 8], br, 5)
-wr = Piece(self, "w", [1, 1], wr, 5)
-bkn = Piece(self, "b", [2, 8], bkn, 3)
-wkn = Piece(self, "w", [2, 1], wkn, 3)
-bb = Piece(self, "b", [3, 8], bb, 3)
-wb = Piece(self, "w", [3, 1], wb, 3)
-bq = Piece(self, "b", [4, 8], bq, 9)
-wq = Piece(self, "w", [4, 1], wq, 9)
-bk = Piece(self, "b", [5, 8], bk, 99999)
-wk = Piece(self, "w", [5, 1], wk, 99999)
-bb2 = Piece(self, "b", [6, 8], bb2, 3)
-wb2 = Piece(self, "w", [6, 1], wb2, 3)
-bkn2 = Piece(self, "b", [7, 8], bkn2, 3)
-wkn2 = Piece(self, "w", [7, 1], wkn2, 3)
-br2 = Piece(self, "b", [8, 8], br2, 5)
-wr2 = Piece(self, "w", [8, 1], wr2, 5)
+#PAWNS
+bp1 = Pawn("b", [0, 6], "bp", 1)
+wp1 = Pawn("w", [0, 1], "wp", 1)
+bp2 = Pawn("b", [1, 6], "bp", 1)
+wp2 = Pawn("w", [1, 1], "wp", 1)
+bp3 = Pawn("b", [2, 6], "bp", 1)
+wp3 = Pawn("w", [2, 1], "wp", 1)
+bp4 = Pawn("b", [3, 6], "bp", 1)
+wp4 = Pawn("w", [3, 1], "wp", 1)
+bp5 = Pawn("b", [4, 6], "bp", 1)
+wp5 = Pawn("w", [4, 1], "wp", 1)
+bp6 = Pawn("b", [5, 6], "bp", 1)
+wp6 = Pawn("w", [5, 1], "wp", 1)
+bp7 = Pawn("b", [6, 6], "bp", 1)
+wp7 = Pawn("w", [6, 1], "wp", 1)
+bp8 = Pawn("b", [7, 6], "bp", 1)
+wp8 = Pawn("w", [7, 1], "wp", 1)
+
+#THE REST
+br = Rook("b", [0, 7], "br", 5)
+wr = Rook("w", [0, 0], "wr", 5)
+bkn = Knight("b", [1, 7], "bkn", 3)
+wkn = Knight("w", [1, 0], "wkn", 3)
+bb = Bishop("b", [2, 7], "bb", 3)
+wb = Bishop("w", [2, 0], "wb", 3)
+bq = Queen("b", [3, 7], "bq", 9)
+wq = Queen("w", [3, 0], "wq", 9)
+bk = King("b", [4, 7], "bk", 99999)
+wk = King("w", [4, 0], "wk", 99999)
+bb2 = Bishop("b", [5, 7], "bb2", 3)
+wb2 = Bishop("w", [5, 0], "wb2", 3)
+bkn2 = Knight("b", [6, 7], "bkn2", 3)
+wkn2 = Knight("w", [6, 0], "wkn2", 3)
+br2 = Rook("b", [7, 7], "br2", 5)
+wr2 = Rook("w", [7, 0], "wr2", 5)
 
 
 board = Chess_Board()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Chess_Board():
+#     def __init__(self):
+#         board = [['    '] * 8] * 8
+#         self.displayBoard(board)
+        
+#     def displayBoard(self, board):
+#         rowNum = 8
+#         print("  ", end="")
+#         for row in board:
+#             print("-" * 41)
+#             print(str(rowNum), end=" ")
+#             rowNum -= 1
+#             for column in row:
+#                 print("|{}".format(column), end="")
+#             print("|")
+#             print("  ", end="")
+#         print("-" * 41)
+#         for letter in ["a", "b", "c", "d", "e", "f", "g", "h"]:
+#             print("   {} ".format(letter), end="")
+#         print("\n")
