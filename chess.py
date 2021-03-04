4e9da8e6de1b26395f95c090b30ebd7177a7b72f
rwang <you@example.com>
2021-03-04 04:36:52 -0500

    added pieces to board

--- chess.py
+++ chess.py
@@ -12,12 +12,18 @@
 class Piece():
     #initalize piece
     def __init__(self, location, displayValue):
-        startLocation = location
-        print(location)
+        #startLocation = location
+        #print(location)
+        self.location = location
+        self.displayValue = displayValue
         
 class Pawn(Piece):
     #define movement to pass to move
     def __init__(self, side, location, displayValue, scoreValue):
+        self.side = side
+        self.location = location
+        self.displayValue = displayValue
+        self.scoreValue = scoreValue
         super().__init__(location, displayValue)
 
     def move(self):
@@ -63,20 +69,23 @@ class King(Piece):
     def move(self):
         print('placeholder for move')
 
-
 class Chess_Board():
-    def __init__(self):
+    #def __init__(self, bp1, wp1, bp2, wp2, bp3, wp3, bp4, wp4, bp5, wp5, bp6, wp6, bp7, wp7, bp8, wp8, br, wr, bkn, wkn, bb, wb, bq, wq, bk, wk, bb2, wb2, bkn2, wkn2, br2, wr2):
+    def __init__(self, pieces):
         board = []
         for row in range(8):
             column = []
             board.append(column)
             for columnNum in range(8):
-                column.append('.')
-        board[0][7] = 'pawn'
-        #board[1][2] = 'pawn'
-        print(board)
-        #self.displayBoard(board)
+                column.append('  ')
         
+        for piece in pieces:
+            index = pieces.index(piece)
+            pieceLocation = pieces[index].location
+            board[pieceLocation[1]][pieceLocation[0]] = str(pieces[index].displayValue).format()
+            #print(pieces[index].side, pieces[index].location, pieces[index].displayValue, pieces[index].scoreValue)
+        self.displayBoard(board)
+
     def displayBoard(self, board):
         rowNum = 8
         print("  ", end="")
@@ -85,7 +94,7 @@ class Chess_Board():
             print(str(rowNum), end=" ")
             rowNum -= 1
             for column in row:
-                print("|{}".format(column), end="")
+                print("| {} ".format(column), end="")
             print("|")
             print("  ", end="")
         print("-" * 41)
@@ -101,43 +110,44 @@ class Chess_Board():
 # Queen: 9 points
 #Create pieces
 #PAWNS
-bp1 = Pawn("b", [0, 6], "bp", 1)
-wp1 = Pawn("w", [0, 1], "wp", 1)
-bp2 = Pawn("b", [1, 6], "bp", 1)
-wp2 = Pawn("w", [1, 1], "wp", 1)
-bp3 = Pawn("b", [2, 6], "bp", 1)
-wp3 = Pawn("w", [2, 1], "wp", 1)
-bp4 = Pawn("b", [3, 6], "bp", 1)
-wp4 = Pawn("w", [3, 1], "wp", 1)
-bp5 = Pawn("b", [4, 6], "bp", 1)
-wp5 = Pawn("w", [4, 1], "wp", 1)
-bp6 = Pawn("b", [5, 6], "bp", 1)
-wp6 = Pawn("w", [5, 1], "wp", 1)
-bp7 = Pawn("b", [6, 6], "bp", 1)
-wp7 = Pawn("w", [6, 1], "wp", 1)
-bp8 = Pawn("b", [7, 6], "bp", 1)
-wp8 = Pawn("w", [7, 1], "wp", 1)
+wp1 = Pawn("w", [0, 6], "wp", 1)
+wp2 = Pawn("w", [1, 6], "wp", 1)
+wp3 = Pawn("w", [2, 6], "wp", 1)
+wp4 = Pawn("w", [3, 6], "wp", 1)
+wp5 = Pawn("w", [4, 6], "wp", 1)
+wp6 = Pawn("w", [5, 6], "wp", 1)
+wp7 = Pawn("w", [6, 6], "wp", 1)
+wp8 = Pawn("w", [7, 6], "wp", 1)
+
+bp1 = Pawn("b", [0, 1], "bp", 1)
+bp2 = Pawn("b", [1, 1], "bp", 1)
+bp3 = Pawn("b", [2, 1], "bp", 1)
+bp4 = Pawn("b", [3, 1], "bp", 1)
+bp5 = Pawn("b", [4, 1], "bp", 1)
+bp6 = Pawn("b", [5, 1], "bp", 1)
+bp7 = Pawn("b", [6, 1], "bp", 1)
+bp8 = Pawn("b", [7, 1], "bp", 1)
 
 #THE REST
-br = Rook("b", [0, 7], "br", 5)
-wr = Rook("w", [0, 0], "wr", 5)
-bkn = Knight("b", [1, 7], "bkn", 3)
-wkn = Knight("w", [1, 0], "wkn", 3)
-bb = Bishop("b", [2, 7], "bb", 3)
-wb = Bishop("w", [2, 0], "wb", 3)
-bq = Queen("b", [3, 7], "bq", 9)
-wq = Queen("w", [3, 0], "wq", 9)
-bk = King("b", [4, 7], "bk", 99999)
-wk = King("w", [4, 0], "wk", 99999)
-bb2 = Bishop("b", [5, 7], "bb2", 3)
-wb2 = Bishop("w", [5, 0], "wb2", 3)
-bkn2 = Knight("b", [6, 7], "bkn2", 3)
-wkn2 = Knight("w", [6, 0], "wkn2", 3)
-br2 = Rook("b", [7, 7], "br2", 5)
-wr2 = Rook("w", [7, 0], "wr2", 5)
+wr = Rook("w", [0, 7], "wr", 5)
+wkn = Knight("w", [1, 7], "wn", 3)
+wb = Bishop("w", [2, 7], "wb", 3)
+wq = Queen("w", [3, 7], "wq", 9)
+wk = King("w", [4, 7], "wk", 99999)
+wb2 = Bishop("w", [5, 7], "wb", 3)
+wkn2 = Knight("w", [6, 7], "wn", 3)
+wr2 = Rook("w", [7, 7], "wr", 5)
 
+br = Rook("b", [0, 0], "br", 5)
+bkn = Knight("b", [1, 0], "bn", 3)
+bb = Bishop("b", [2, 0], "bb", 3)
+bq = Queen("b", [3, 0], "bq", 9)
+bk = King("b", [4, 0], "bk", 99999)
+bb2 = Bishop("b", [5, 0], "bb", 3)
+bkn2 = Knight("b", [6, 0], "bn", 3)
+br2 = Rook("b", [7, 0], "br", 5)
 
-board = Chess_Board()
+board = Chess_Board([bp1, wp1, bp2, wp2, bp3, wp3, bp4, wp4, bp5, wp5, bp6, wp6, bp7, wp7, bp8, wp8, br, wr, bkn, wkn, bb, wb, bq, wq, bk, wk, bb2, wb2, bkn2, wkn2, br2, wr2])
 
 
 
